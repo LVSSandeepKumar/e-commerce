d40c0d8745f06b240102cb45cc0c6367a0e21061
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-10-01 11:17:33 +0530

    Frontend routing using react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
 import * as React from "react";
 
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+
 function App() {
 
   return (
-    <div className="bg-red-500">
-      Hello
+    <div className="">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />}/>
+      </Routes>
     </div>
   )
 }
